00bcb0add7c2f049d1048a22500117a1a8a9fdc0
AnanthapalliAvinash <avinashananthapalli11@gmail.com>
2022-01-10 14:34:59 +0530

    StringReverse

---
+++ FibnacciSeries/Program.cs
@@ -0,0 +1,24 @@
+﻿using System;
+
+namespace FibnacciSeries
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int n1 = 0, n2 = 1, n3, i, n;
+            Console.Write("Enter the number of elements");
+            n = int.Parse(Console.ReadLine());
+            Console.Write(n1 +" ");   
+            Console.Write(n2 +" ");
+            for (i = 2;i<n;i++)    
+            {
+                n3 = n1 + n2;
+                Console.Write(n3 + " ");
+                n1 = n2;
+                n2 = n3;
+            }
+            Console.ReadLine();
+        }
+    }
+}
---
+++ StringReverse/Program.cs
@@ -0,0 +1,28 @@
+﻿using System;
+
+namespace StringReverse
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string str, s = "";  
+            int l;               
+            str = Console.ReadLine();           
+
+            l = str.Length - 1;           
+            while (l >= 0)                
+            {
+
+                s = s + str[l];  
+                l--;
+
+            }
+
+            Console.WriteLine("Reverse of a string is{0}", s); 
+            Console.ReadLine();                                                
+        }
+    }
+}
+    
+
